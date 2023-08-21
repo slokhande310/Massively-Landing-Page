bc64a6ac37e27793152adda2738d73877460057d
Shubham Lokhande <slokhande310@gmail.com>
2023-08-21 19:32:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// use strict mode
+
+const menu_btn = document.querySelector('.menu-btn');
+const menu_bar = document.querySelector('.menu-btn i');
+const sideNav = document.querySelector('.side-nav');
+
+menu_btn.addEventListener('click', toggle);
+
+function toggle(){
+    sideNav.classList.toggle('show');
+    const show = sideNav.classList.contains('show');
+    
+    menu_bar.classList = show ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
