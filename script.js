d264c4c72418a6e09d67a5e8c97cd753f5feb31b
Shubham Lokhande <slokhande310@gmail.com>
2024-01-23 13:30:55 +0530

    fixed: down button not working

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const sideNav = document.querySelector('.side-nav');
 
 menu_btn.addEventListener('click', toggle);
 
-function toggle(){
+function toggle() {
     sideNav.classList.toggle('show');
     const show = sideNav.classList.contains('show');
-    
+
     menu_bar.classList = show ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
+
+function scrollToMain() {
+    const mainElement = document.getElementById('main');
+
+    if (mainElement) {
+        mainElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
 }
